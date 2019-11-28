8410ffabc1dd9f7ad0362975336319bbc3cc5ad8
Jaafar Mahdi <jafa@bang-olufsen.dk>
2019-11-28 10:00:48 +0100

    Create adding_default_values.sql

---
+++ adding_default_values.sql
@@ -0,0 +1,51 @@
+CALL AddPurpose('Tennis');
+CALL AddPurpose('Badminton');
+CALL AddPurpose('Squash');
+
+CALL AddStringType('Polyester');
+CALL AddStringType('Natural Gut');
+CALL AddStringType('Synthetic Gut');
+CALL AddStringType('Multifilament');
+CALL AddStringType('Hybrid');
+
+CALL AddColor('Black');
+CALL AddColor('Blue');
+CALL AddColor('Gold');
+CALL AddColor('Green');
+CALL AddColor('Natural');
+CALL AddColor('Orange');
+CALL AddColor('Pink');
+CALL AddColor('Puple');
+CALL AddColor('Red');
+CALL AddColor('Silver');
+CALL AddColor('Teal');
+CALL AddColor('White');
+CALL AddColor('Yellow');
+
+CALL AddRacketBrand('Babolat');
+CALL AddRacketBrand('Dunlop');
+CALL AddRacketBrand('Gamma');
+CALL AddRacketBrand('Head');
+CALL AddRacketBrand('Prince');
+CALL AddRacketBrand('ProKennex');
+CALL AddRacketBrand('Signum Pro');
+CALL AddRacketBrand('Tecnifibre');
+CALL AddRacketBrand('Volkl');
+CALL AddRacketBrand('Wilson');
+CALL AddRacketBrand('Yonex');
+
+CALL AddBrand('Ashaway');
+CALL AddBrand('Babolat');
+CALL AddBrand('Dunlop');
+CALL AddBrand('Head');
+CALL AddBrand('Gamma');
+CALL AddBrand('Kirschbaum');
+CALL AddBrand('Luxilon');
+CALL AddBrand('Prince');
+CALL AddBrand('Signum Pro');
+CALL AddBrand('Solinco');
+CALL AddBrand('Tecnifibre');
+CALL AddBrand('Volkl');
+CALL AddBrand('Wilson');
+CALL AddBrand('Yonex');
+
