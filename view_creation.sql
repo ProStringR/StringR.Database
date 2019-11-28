68b029e55d6f0f6ebdf36f4b79aec177f665c311
Jaafar Mahdi <jafa@bang-olufsen.dk>
2019-11-28 12:03:03 +0100

    Update View according to demands from developers

--- view_creation.sql
+++ view_creation.sql
@@ -23,18 +23,25 @@ SELECT
 	Stringers.Email AS stringerEmail,
 	RacketBrands.RacketBrand AS racketBrand,
 	RacketModels.RacketModel AS racketModel,
-	Rackets.Brand AS racketBrandId,
-	Rackets.Model AS racketModelId,
-	StringInfo.Model AS stringModel,
-	StringInfo.StringType AS stringType,
-	StringInfo.Brand AS stringBrand,
-	StringInfo.Thickness AS thickness,
-	StringInfo.Purpose AS purpose,
-	StringInfo.Color AS color
+	Rackets.Weight AS racketWeight,
+	Rackets.StringsMain AS racketMain,
+	Rackets.StringsCross AS racketCross,
+	Rackets.GripSize AS racketGripSize,
+	StringModels.StringModel AS stringModel,
+	StringTypes.StringType AS stringType,
+	Brands.Brand AS stringBrand,
+	StringInfo.Thickness AS stringThickness,
+	Purposes.Purpose AS stringPurpose,
+	Colors.Color AS stringColor
 	FROM Orders
 	JOIN Customers ON Orders.Customer = Customers.Id
 	JOIN Stringers ON Orders.Stringer = Stringers.Id
 	JOIN Rackets ON Orders.Racket = Rackets.Id
 	JOIN RacketBrands ON Rackets.Brand = RacketBrands.Id
 	JOIN RacketModels ON Rackets.Model = RacketModels.Id
-	JOIN StringInfo ON Orders.StringId = StringInfo.RacketStringId;
+	JOIN StringInfo ON Orders.StringId = StringInfo.RacketStringId
+	JOIN StringModels ON StringInfo.Model = StringModels.Id
+	JOIN StringTypes ON StringInfo.StringType = StringTypes.Id
+	JOIN Brands ON StringInfo.Brand = Brands.Id
+	JOIN Purposes ON StringInfo.Purpose = Purposes.Id
+	JOIN Colors ON StringInfo.Color = Colors.Id
